8f159ddb0acdf6517789f81e78ba1cf18ac1daa5
Nethasa Rupasinghe <nethasarupasinghe@gmail.com>
2026-04-02 19:34:14 +0530

    fetch rewards from backend API

--- frontend/src/pages/Rewards.jsx
+++ frontend/src/pages/Rewards.jsx
@@ -1,82 +1,25 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RewardCard from "../components/RewardCard";
+import API from "../services/api";
 
 export default function Rewards() {
-    //Temp static data later we can connect API
-    const rewards = [
-        {
-            _id: 1,
-            title: "Chocolate Muffin",
-            pointsRequired: 50,
-            image: "https://www.shutterstock.com/image-photo/closeup-chocolate-muffin-isolated-on-600nw-2483081425.jpg"
-        },
-        {
-            _id: 2,
-            title: "Croissant",
-            pointsRequired: 40,
-            image: "https://dylanpatisserie.com/cdn/shop/files/croisant.png?v=1764334087"
-        },
-        {
-            _id: 3,
-            title: "Macaron box",
-            pointsRequired: 80,
-            image: "https://packagingbee.co.uk/wp-content/uploads/2022/03/macaron-packaging-ideas.jpg"
-        },
-        {
-            _id: 4,
-            title: "Chocolate Muffin",
-            pointsRequired: 50,
-            image: "https://www.shutterstock.com/image-photo/closeup-chocolate-muffin-isolated-on-600nw-2483081425.jpg"
-        },
-        {
-            _id: 5,
-            title: "Croissant",
-            pointsRequired: 40,
-            image: "https://dylanpatisserie.com/cdn/shop/files/croisant.png?v=1764334087"
-        },
-        {
-            _id: 6,
-            title: "Macaron box",
-            pointsRequired: 80,
-            image: "https://packagingbee.co.uk/wp-content/uploads/2022/03/macaron-packaging-ideas.jpg"
-        },
-        {
-            _id: 7,
-            title: "Chocolate Muffin",
-            pointsRequired: 50,
-            image: "https://www.shutterstock.com/image-photo/closeup-chocolate-muffin-isolated-on-600nw-2483081425.jpg"
-        },
-        {
-            _id: 8,
-            title: "Croissant",
-            pointsRequired: 40,
-            image: "https://dylanpatisserie.com/cdn/shop/files/croisant.png?v=1764334087"
-        },
-        {
-            _id: 9,
-            title: "Macaron box",
-            pointsRequired: 80,
-            image: "https://packagingbee.co.uk/wp-content/uploads/2022/03/macaron-packaging-ideas.jpg"
-        },
-        {
-            _id: 10,
-            title: "Chocolate Muffin",
-            pointsRequired: 50,
-            image: "https://www.shutterstock.com/image-photo/closeup-chocolate-muffin-isolated-on-600nw-2483081425.jpg"
-        },
-        {
-            _id: 11,
-            title: "Croissant",
-            pointsRequired: 40,
-            image: "https://dylanpatisserie.com/cdn/shop/files/croisant.png?v=1764334087"
-        },
-        {
-            _id: 12,
-            title: "Macaron box",
-            pointsRequired: 80,
-            image: "https://packagingbee.co.uk/wp-content/uploads/2022/03/macaron-packaging-ideas.jpg"
-        },
-    ];
+    
+    const [rewards, setRewards] = useState([]);
+
+    useEffect(() => {
+        fetchRewards();
+    }, []);
+
+    const fetchRewards = async () => {
+        try {
+            const res = await API.get("/rewards");
+            setRewards(res.data);
+        } catch (error) {
+            console.error("Error fetching rewards", error);
+        }
+    };
+    
 
    return (
     <div className="min-h-screen bg-gradient-to-r from-[#e6a65c] to-[#bfb8ae] p-6">
